3e7d6c785ce6d2ee12c3d9f56ae9dfc73897b200
Jammonjones <wjssaan@gmail.com>
2023-05-26 18:36:18 -0600

    fexed add cart

--- js/product.js
+++ js/product.js
@@ -1,9 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // I edited this function to add an aray of items to local storage insead of just one item it works
+  // on local but not yet on netlify
+  let currentCartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(currentCartItems)){
+  currentCartItems = [];
+  }
+  currentCartItems.push(product);
+  setLocalStorage("so-cart", currentCartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
