fcbd68687a3e15bb648f3fc8cf77318916f3e84f
Jesus Bejar <israelbejarii@gmail.com>
2023-05-30 10:49:28 -0600

    made productDetails() in productDetails.mjs file

---
+++ js/productDetails.mjs
@@ -0,0 +1,13 @@
+// This page will be responsible for all 
+// of the functionality needed 
+// to lookup data for a specific 
+// product and display it in HTML
+// it follows the black box philosophy
+
+// productDetails() is the entrypoint  
+// into our module and will make sure 
+// that everything happens 
+// in the right order
+export default function productDetails(productId) {
+    return productId + 1
+};
