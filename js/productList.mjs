20a650ce986169637c0ba55769352342854d6af1
Jesus Bejar <israelbejarii@gmail.com>
2023-06-03 20:33:04 -0600

    finished productList function

--- js/productList.mjs
+++ js/productList.mjs
@@ -1,6 +1,11 @@
-import {getData} from "./productData.mjs";
+import {getData} from "../js/productData.mjs";
 // the function below generates 
 // a list of product cards in HTML from an array.
 export default async function productList(selector, category) {
-    
+    // get the element we will insert the list into from the selector
+    let theSelector = document.querySelector(selector);
+    // get the list of products 
+    let products = await getData(category);
+    console.log(products);
+    // render out the product list to the element
 }
