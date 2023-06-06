189c68339f599eead0e27e89083c0096921e1dd2
Jesus Bejar <israelbejarii@gmail.com>
2023-06-06 09:26:59 -0600

    made shoppingCart()

--- cart/shoppingCart.mjs
+++ cart/shoppingCart.mjs
@@ -1,1 +1,8 @@
 import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+
+export default function ShoppingCart() {
+    const cartItems = getLocalStorage("so-cart");
+    const outputEl = document.querySelector(".product-list");
+    renderListWithTemplate(cartItemTemplate, outputEl, cartItems);
+  }
+  
